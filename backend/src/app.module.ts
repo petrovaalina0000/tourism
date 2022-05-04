f3895816e447b53d320d56103b86408faa32787e
John Doe <johndoe@example.com>
2022-05-04 23:45:39 +0300

    fixed package.json

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,7 +7,7 @@ import { join } from 'path';
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname,  'build'),
+      rootPath: join(__dirname, 'build'),
     }),
   ],
   controllers: [AppController],
